c563f32e7ea3f28111d5d834698e46f15430cf6d
peterix <fork@kuelzer.me>
2020-04-22 09:12:50 +0200

    add logging extension, add methods to load data files

---
+++ src/Util/Extensions/LoggerExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Vintagestory.API.Common;
+
+namespace Foundation.Util.Extensions
+{
+    public static class LoggerExtensions
+    {
+        public static void LogRaw(this ILogger logger, EnumLogType logType, string message)
+        {
+            logger.Log(logType, "{0}", message);
+        }
+    }
+}
